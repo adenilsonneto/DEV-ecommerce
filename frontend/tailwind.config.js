ddd9671b3b468862883f6fa2dcecf978153a3a4d
adenilsonneto <adenetoo333@gmail.com>
2026-04-14 16:31:27 -0300

    chore: configura Tailwind CSS e estrutura de pastas

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+content: [
+"./index.html",
+"./src/**/*.{js,ts,jsx,tsx}", //diz ao arquivo onde procurar classes
+],
+theme: {
+extend: {},
+},
+plugins: [],
+}
